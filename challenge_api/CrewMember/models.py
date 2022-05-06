40ed22431971c16a6f2228ead8641052518d0b18
akrammukasa <akram@bposeats.com>
2022-05-06 17:14:10 +0300

    Work on the model relationships

--- challenge_api/CrewMember/models.py
+++ challenge_api/CrewMember/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from helpers.models import BaseAbstractModel
+from Ship.models import Ship
 
 
 
 
 class CrewMember(BaseAbstractModel):
     name = models.CharField(max_length=255, blank=True, null=True)
+    ship = models.ForeignKey(Ship,on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.name
--- challenge_api/Ship/models.py
+++ challenge_api/Ship/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 from helpers.models import BaseAbstractModel
+from Mothership.models import Mothership
+
 
 # Create your models here.
 
 
 class Ship(BaseAbstractModel):
     name = models.CharField(max_length=255, blank=True, null=True)
+    mothership =  models.ForeignKey(Mothership, on_delete=models.CASCADE, null=True)
     
     def __str__(self):
         return self.name
